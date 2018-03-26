aae348cdd3b5321a30c9cb42b205f4c384ca9ceb
ioanachi <ioana_chichi@outlook.com>
2018-03-26 16:11:32 +0300

    name validation

---
+++ src/js/mainContainer.js
@@ -0,0 +1,12 @@
+
+var nameField = document.myForm.name;
+
+
+var submitBtn = document.getElementById('submitBtn');
+submitBtn.addEventListener('click', function(){
+    console.log(nameField.value);
+
+if(){
+
+}
+})
